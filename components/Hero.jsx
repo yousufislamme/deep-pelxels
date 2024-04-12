1a74bb01be9351c4ad26a53edb7a3fa2c0f642ab
yousufislamme <online.okk@gmail.com>
2024-04-12 20:46:12 +0600

    install prettier

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     "The best free stock photos, royalty free images & videos shared by creators.";
   return (
     <div className="w-full">
-      <div className=" flex h-[30rem] w-full flex-col items-center justify-center border bg-pink-300 bg-hero-pattern bg-no-repeat object-cover">
+      <div className="flex h-[30rem] w-full flex-col items-center justify-center border bg-pink-300 bg-hero-pattern bg-no-repeat object-cover">
         <h1 className="mx-auto w-[600px] text-center font-sans text-3xl font-bold text-white ">
           {headingText}
         </h1>
